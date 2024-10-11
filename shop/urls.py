33e10ea622f3f3d0d28ac598adca333f85959485
sergio <sergey.rybakov@mail.ru>
2024-10-11 19:50:47 +0300

    init

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, CategoryCreateView
 
 urlpatterns = [
     path('product/', ProductListView.as_view(), name='products'),
+
+    path('categories/add/', CategoryCreateView.as_view(), name='category_add'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.views.generic import (ListView, CreateView,
                                   UpdateView, DeleteView,
                                     DetailView)
-from .models import Product
+
+from django.urls import reverse_lazy
+
+from .models import Product, Category
 
 class ProductListView(ListView):
     model = Product
     template_name = 'shop/products.html'
     context_object_name = 'products'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = '__all__'
+    form_class = CategoryCreateForm
+    template_name = 'shop/category_add.html'
+    success_url = reverse_lazy('categories')
 
